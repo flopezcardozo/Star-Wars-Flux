6bd8a7e36466a92f7802b2bf00d337bc8afe6712
flopezcardozo <flopezcardozo@gmail.com>
2021-10-01 15:33:44 +0000

    cards alineadas

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -2,37 +2,37 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
-
+import Proptypes from "prop-types";
 import "../../styles/demo.scss";
 
-export const Demo = () => {
+export const Demo = props => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
-			key={index}
-			{store.planets.map((item, index) => {
-				return (
-					
-					<div className="card" style={{ width: "18rem" }}>
-						<img src="..." className="card-img-top" alt="..." />
-						<div className="card-body">
+		<div className="row">
+			<div className="row flex-row flex-nowrap">
+				{store.planets.map((item, index) => {
+					return (
+						<div key={index} className="card" style={{ width: "18rem" }}>
+							<img src="..." className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">{item.name}</h5>
 
+								<p>Population: {item.population}</p>
+								<p>Terrain: {item.terrain}</p>
+							</div>
+							<br />
 							<Link to={"/single/" + index}>
-								<span>Link to: {item.name}</span>
+								<button className="btn btn-primary">Learn More</button>
 							</Link>
-
 						</div>
-						<br />
-						<Link to="/">
-							<button className="btn btn-primary">Back home</button>
-						</Link>
-					</div>
-				
-				),}};
-		
-
+					);
+				})}
+			</div>
 		</div>
 	);
-
 };
+// Demo.propTypes = {
+// 	population: Proptypes.number,
+// 	terrain: Proptypes.String
+// };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Demo } from "./demo";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	return (
+		<div className="container-fluid">
+			<h2>Planets</h2>
+			<div className="row flex-row flex-nowrap">
+				<Demo />
+				<a href="#" className="btn btn-success">
+					If you see this green button, bootstrap is working
+				</a>
+			</div>
+		</div>
+	);
+};
